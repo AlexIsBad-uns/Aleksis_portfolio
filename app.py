74f33da10f8256143a266fd46e225a0b148c9014
AlexIsBad-uns <badunsaleksis@gmail.com>
2024-10-29 19:25:43 +0200

    App and html cleanup

--- app.py
+++ app.py
@@ -3,18 +3,25 @@ import time
 import smtplib
 import os
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
 load_dotenv()
 app = Flask(__name__)
 
+
+# Sūtīt e-pastus
 def mail(email, pssd, mssg):
-    
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(email, pssd)
-    server.sendmail(email, 'badunsaleksis@gmail.com', mssg)
 
+    # utf-16 nodrošina garumzīmes un citus simbolus
+    msg = MIMEText(mssg, 'plain', 'utf-16')
+    msg['From'] = email
+    msg['To'] = 'badunsaleksis@gmail.com'
+    msg['Subject'] = "Jauna zina no Portfolio majaslapas"
+    
+    server.sendmail(email, 'badunsaleksis@gmail.com', msg.as_string())
     server.quit()
-
 
 
 
@@ -29,7 +36,7 @@ def cleanMsg(data):
     msg = data.get('message')
     message = f"""
         From: {name} <{email}>
-        Subject: New Message On Our Official Website
+        Subject: Jauna zina no Portfolio majaslapas
         {msg}
     """
     return message
@@ -43,9 +50,9 @@ def form():
         mail('badunsaleksis@gmail.com', password, msg)
         return render_template("index.html",code="test()")
     else:
-        return f"Something Not Right "
+        return f"Kautkas neiet"
     
-
+# Problēmu gadījumā 
 @app.errorhandler(404)  
 def not_found(e): 
   return render_template("404.html") 
